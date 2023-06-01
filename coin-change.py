4a951b76d7c5f25ff374d3d9557744205c146d80
debiB <84740374+debiB@users.noreply.github.com>
2023-06-01 08:01:34 -0700

    added coin-change.python

--- coin-change.py
+++ coin-change.py
@@ -1,15 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        d = defaultdict(int)
-
-        def dp(target):
-            if target < 0:
-                return float("inf")
-            if target == 0:
-                return 0
-            if target not in d:
-                d[target] = float("inf")
-                for i in coins:
-                    d[target] = min(d[target],dp(target-i) +1)
-            return d[target]   
-        return  dp(amount) if  dp(amount) != float("inf") else -1
+        dp= [float("inf") for _ in range(amount+1)]
+        dp[0] = 0 
+        
+        for i in range(1,amount+1):
+            for j in coins:
+              if i-j >= 0:  
+                dp[i] = min(dp[i], dp[i-j] + 1) 
+        return dp[-1] if dp[-1] != inf else -1
