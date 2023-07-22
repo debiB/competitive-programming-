226c8810f79bf8695d4bd780d62c14ba770fcadc
debiB <84740374+debiB@users.noreply.github.com>
2023-07-22 19:57:27 +0300

    added split-array-into-consecutive-subsequences.python

--- split-array-into-consecutive-subsequences.py
+++ split-array-into-consecutive-subsequences.py
@@ -1,25 +1,17 @@
 class Solution:
     def isPossible(self, nums):
-        freq_map = {}
-        end_map = {}
-
-        for num in nums:
-            freq_map[num] = freq_map.get(num, 0) + 1
-
-        for num in nums:
-            if freq_map[num] == 0:
-                continue
-
-            freq_map[num] -= 1
-
-            if end_map.get(num - 1, 0) > 0:
-                end_map[num - 1] -= 1
-                end_map[num] = end_map.get(num, 0) + 1
-            elif freq_map.get(num + 1, 0) > 0 and freq_map.get(num + 2, 0) > 0:
-                freq_map[num + 1] -= 1
-                freq_map[num + 2] -= 1
-                end_map[num + 2] = end_map.get(num + 2, 0) + 1
-            else:
-                return False
-
+        freq = collections. Counter(nums)
+        end = defaultdict(int)
+        for i in nums:
+           if freq[i] > 0:
+                freq[i]-=1
+                if end[i-1] > 0:
+                   end[i-1]-=1
+                   end[i]+=1
+                elif freq[i+1] > 0 and freq[i+2] > 0:
+                    freq[i+1]-=1
+                    freq[i+2]-=1
+                    end[i+2]+=1
+                else:
+                    return False
         return True
