52ad9f8321faf3f398799ce7f170c66b03238e7b
Dibora Berhanu <84740374+debiB@users.noreply.github.com>
2023-10-10 18:15:26 +0300

    added validate-binary-search-tree.python

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -5,12 +5,13 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helper(self, root, lower_bound, upper_bound):
-        if not root:
-            return True
-        elif(root.val <= lower_bound or root.val >= upper_bound):
-            return False
-        else:
-            return self.helper(root.left, lower_bound, root.val) and self.helper(root.right, root.val, upper_bound)
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-       return self.helper(root, -inf, inf)
+        
+        def helper(root, lower_bound, upper_bound):
+            if not root:
+                return True
+            if (root.val >= upper_bound) or (root.val <= lower_bound):
+                return False 
+            else:
+                return helper(root.left, lower_bound,root.val) and helper(root.right, root.val, upper_bound)
+        return helper(root,-inf,inf)
