bbdb9ff8bfbc83188bba72f76dfce51bb20efcf0
Dibora Berhanu <84740374+debiB@users.noreply.github.com>
2023-10-12 15:25:16 +0300

    added design-hashmap.python

---
+++ design-hashmap.py
@@ -0,0 +1,24 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.d = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.d[key] = value
+
+    def get(self, key: int) -> int:
+        if key in self.d:
+            return self.d.get(key)
+        else:
+            return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.d:
+            del self.d[key]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
