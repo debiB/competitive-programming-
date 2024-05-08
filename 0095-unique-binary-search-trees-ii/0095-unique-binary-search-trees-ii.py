1c130af06ef39fb52ebb2445c7b4637bf2ddf118
Dibora Berhanu <84740374+debiB@users.noreply.github.com>
2024-05-08 11:21:07 +0300

    Time: 46 ms (68.34%), Space: 17.3 MB (74.64%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -6,14 +6,18 @@
 #         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        dp = defaultdict(list)
         def generate(left, right):
             if left > right:
                 return [None]
+            if (left,right) in dp:
+                return dp[(left,right)]
             res = []
             for val in range(left, right+1):
                 for leftTree in generate(left, val-1):
                     for rightTree in generate(val+1,right):
                         root = TreeNode(val, leftTree, rightTree)
                         res.append(root)
+                        dp[(left,right)].append(root)
             return res
         return generate(1,n)
